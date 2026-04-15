c0aa1de7f35fd4892d414623f2d60f340d81302d
javedmalik <javedinsha.malik@gmail.com>
2026-04-15 18:30:56 +0530

    Initial commits final commit

--- next.config.js
+++ next.config.js
@@ -13,28 +13,28 @@ const nextConfig = {
           { key: "X-DNS-Prefetch-Control", value: "on" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
           { key: "X-Frame-Options", value: "SAMEORIGIN" },
-          // { key: "X-Content-Type-Options", value: "nosniff" },
-          // { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          // {
-          //   key: "Permissions-Policy",
-          //   value: "camera=(), microphone=(), geolocation=(self)",
-          // },
-          // {
-          //   key: "Strict-Transport-Security",
-          //   value: "max-age=63072000; includeSubDomains; preload",
-          // },
-          // {
-          //   key: "Content-Security-Policy",
-          //   value: [
-          //     "default-src 'self'",
-          //     "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-          //     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-          //     "font-src 'self' https://fonts.gstatic.com",
-          //     "img-src 'self' data: blob: https:",
-          //     "connect-src 'self'",
-          //     "frame-ancestors 'none'",
-          //   ].join("; "),
-          // },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(self)",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https:",
+              "connect-src 'self'",
+              "frame-ancestors 'none'",
+            ].join("; "),
+          },
         ],
       },
     ];
